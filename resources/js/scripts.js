56cae8649afe2894d86c53746bbd3e6c0de83ce6
romrauq <johnequarmor@gmail.com>
2025-01-25 16:01:05 +0000

    Prepared script that changes the elements displayed within a button when the add to cart button is clicked.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,8 +1,5 @@
 // Define DOM element variables:
 let desserts_items_container = document.getElementById("dessert-items-container");
-let add_to_cart_buttons = document.getElementsByClassName("add-to-cart-button");
-let default_button_array = document.getElementsByClassName("button-content-default");
-let dynamic_button_array = document.getElementsByClassName("button-content-dynamic");
 let empty_cart_image = document.getElementById("empty-cart-image");
 let empty_cart_text = document.getElementById("empty-cart-text");
 
@@ -16,47 +13,63 @@ fetch("../data/data.json")
 		for (let index = 0; index < data.length; index++) {
 			const data_item = data[index];
 
+			// Create a new dessert item element
 			const dessert_item = document.createElement("div");
+			dessert_item.className = "dessert-item"; // Optional: Add a class for styling
 			dessert_item.innerHTML = `
-		<div class="image-button-container">
-							<img
-								src="${data_item["image"]["desktop"]}"
-								alt=""
-								class="dessert-image"
-								id="dessert-image"
-							/>
-							<button>
-								<div class="button-content-default">
-									<img
-										src="./resources/images/icon-add-to-cart.svg"
-										alt=""
-										class="add-to-cart-button"
-										id="add-to-cart-button"
-									/>Add to Cart
-								</div>
+          <div class="image-button-container">
+            <img
+              src="${data_item["image"]["desktop"]}"
+              alt=""
+              class="dessert-image"
+              id="dessert-image"
+            />
+            <button class="add-to-cart-button">
+              <div class="button-content-default">
+                <img
+                  src="./resources/images/icon-add-to-cart.svg"
+                  alt=""
+                  class="add-to-cart-icon"
+                />Add to Cart
+              </div>
+              <div class="button-content-dynamic" style="display: none;">
+                <img
+                  src="./resources/images/icon-decrement-quantity.svg"
+                  alt=""
+                  class="count-icon decrement-icon"
+                />
+                <span class="item-button-count">1</span>
+                <img
+                  src="./resources/images/icon-increment-quantity.svg"
+                  alt=""
+                  class="count-icon increment-icon"
+                />
+              </div>
+            </button>
+          </div>
+          <p class="dessert-type">${data_item.category}</p>
+          <p class="dessert-specific">${data_item.name}</p>
+          <p class="dessert-price">$${parseFloat(data_item.price).toFixed(2)}</p>
+      `;
 
-								<div class="button-content-dynamic">
-									<img
-										src="./resources/images/icon-decrement-quantity.svg"
-										alt=""
-										class="count-icon decrement-icon"
-									/>
-									<span class="item-button-count">4</span>
-									<img
-										src="./resources/images/icon-increment-quantity.svg"
-										alt=""
-										class="count-icon increment-icon"
-									/>
-								</div>
-							</button>
-						</div>
-
-						<p class="dessert-type" id="dessert-type">${data_item.category}</p>
-						<p class="dessert-specific" id="dessert-specific">${data_item.name}</p>
-						<p class="dessert-price" id="dessert-price">$${parseFloat(data_item.price).toFixed(2)}</p>
-	`;
-			// Dynamically render (all) dessert item elements into container element:
+			// Append the dessert item to the container
 			desserts_items_container.appendChild(dessert_item);
+
+			// Add event listener to the "Add to Cart" button
+			const add_to_cart_button = dessert_item.querySelector(".add-to-cart-button");
+			const default_button = dessert_item.querySelector(".button-content-default");
+			const dynamic_button = dessert_item.querySelector(".button-content-dynamic");
+
+			add_to_cart_button.addEventListener("click", () => {
+				// Change button background color:
+				add_to_cart_button.style.backgroundColor = "hsl(14, 86%, 42%)";
+
+				// Hide the default button content
+				default_button.style.display = "none";
+
+				// Show the dynamic button content
+				dynamic_button.style.display = "flex";
+			});
 		}
 	})
 	.catch((error) => console.error("Error:", error));
